4f56880e405b445f7277b0c920406db4cc83455d
saibing <saibingdeng@gmail.com>
2018-09-23 22:56:24 +0800

    add hover docs

---
+++ pkg2/x.go
@@ -0,0 +1,20 @@
+// Package pkg2 shows dependencies.
+//
+// How to
+//
+// 	Example Code!
+//
+package pkg2
+
+// A comment that should be ignored
+
+// X does the unknown.
+func X() {
+	panic("zomg")
+}
+
+// Header is like an HTTP header, only better.
+type Header struct {
+	// F is a string, too.
+	F string
+}
